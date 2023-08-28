9fc6fedc4327fa6649e459c1db0b4e721fedc61b
Marco <marco.mojica@hotmail.de>
2023-08-28 20:24:30 +0200

    Adjust fog distance

--- components/scene.tsx
+++ components/scene.tsx
@@ -26,7 +26,7 @@ export default function Scene({ words, setWords }: SceneProps) {
       }}
       frameloop="demand"
     >
-      <fog attach="fog" color={"black"} near={0} far={80} />
+      <fog attach="fog" color="black" near={0} far={100} />
       {/* <OrbitControls enableZoom enablePan enableRotate /> */}
       <ambientLight intensity={0.2} />
       <pointLight position={[15, 0, -10]} intensity={200} color={0x00ff00} />
